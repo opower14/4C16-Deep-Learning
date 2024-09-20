66927a293c9d1f3888bd117a0410c8c67a197347
opower14 <160499982+opower14@users.noreply.github.com>
2024-09-20 14:50:47 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+def say_stuff():        # define function
+  print("some day")     # spaces at the start (typically 2 or 4)
+  print("when the world is cold")
+  print("I will feel a glow")
+  print("just like thinking of you")
+  print("and the way you look tonight") # to end function add newline (press return)
+
+# new code that is outside of function 
